9c22dcdc6fa1e9850b10133ac9086fc425e2788e
George Shore <georgedshore@gmail.com>
2023-11-29 09:58:04 +0000

    Start of Phase 3 part 2

--- Database_setup_project/databases-in-python-project-starter/app.py
+++ Database_setup_project/databases-in-python-project-starter/app.py
@@ -1,25 +1,45 @@
-from lib.database_connection import DatabaseConnection
+# file: app.py
+
 from lib.artist_repository import ArtistRepository
+from lib.database_connection import DatabaseConnection
 from lib.album_repository import AlbumsRepository
 
+class Application():
+  def __init__(self):
+    self._connection = DatabaseConnection()
+    self._connection.connect()
+    self._connection.seed("seeds/music_library.sql")
 
-# Connect to the database
-connection = DatabaseConnection()
-connection.connect()
+  def run(self):
+    # "Runs" the terminal application.
+    # It might:
+    #   * Ask the user to enter some input
+    #   * Make some decisions based on that input
+    #   * Query the database
+    #   * Display some output
+    # We're going to print out the artists!
+    print('Welcome to the music library manager!\n\n')
+    print('What would you like to do?')
+    print('1 - List all albums')
+    print('2 - List all artists\n')
 
-# Seed with some seed data
-connection.seed("seeds/music_library.sql")
+    
+    choice = int(input('Enter your choice: '))
+    print('\n\n\n')
+    if choice ==1:
+       album_repository = AlbumsRepository(self._connection)
+       albums = album_repository.all()
+       print('Here is a list of albums:')
+       for album in albums:
+          print(f'  * {album.id} - {album.title}')
+    elif choice == 2:
 
-# Retrieve all artists
-artist_repository = ArtistRepository(connection)
-artists = artist_repository.all()
+        artist_repository = ArtistRepository(self._connection)
+        artists = artist_repository.all()
+        print('Here is a list of artists:')
+        for artist in artists:
+            print(f"  * {artist.id}: {artist.name} ({artist.genre})")
 
-# List them out
-for artist in artists:
-    print(artist)
-
-#Retrieve albums in table.
-album_repository = AlbumsRepository(connection)
-albums = album_repository.all()
-for album in albums:
-    print(album)
+if __name__ == '__main__':
+    app = Application()
+    app.run()
